576db007180fd056e5aeb4dc26322dd6a043d7bf
SamSSY <sam7822567@gmail.com>
2016-05-05 09:23:47 +0800

    0505/0923

--- background.js
+++ background.js
@@ -2,7 +2,12 @@ const fbUrl = 'https://www.facebook.com/';
 const youtubeUrl = 'https://www.youtube.com/';
 let count = 0;
 
-chrome.tabs.query({/* active: true, lastFocusedWindow: true*/}, (arrayOfTabs) => {
+// to ensure chrome object loaded correctly
+if (chrome.tabs == undefined){
+  window.location.reload();
+}
+
+chrome.tabs.query({}, (arrayOfTabs) => {
   for ( var i in arrayOfTabs) {
     if (arrayOfTabs[i].url.indexOf(fbUrl) > -1 ||
       arrayOfTabs[i].url.indexOf(youtubeUrl) > -1) {
@@ -17,8 +22,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.url) {
     if (changeInfo.url.indexOf(fbUrl) > -1 ||
       changeInfo.url.indexOf(youtubeUrl) > -1) {
-      console.log(count);
-      if (count > 10) {
+       if (count > 10) {
         chrome.tabs.update(tabId, {url: 'https://stackoverflow.com/'}, () => {
           // count = 0;
         });
